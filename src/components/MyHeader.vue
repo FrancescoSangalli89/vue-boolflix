8a0478a38cbdecb8a589bea7bb6536cebe2df6d4
Francesco Sangalli <francesco.sangalli89@gmail.com>
2022-08-30 22:21:09 +0200

    start: milestone1

---
+++ src/components/MyHeader.vue
@@ -0,0 +1,21 @@
+<template>
+    <header>
+        <input type="text" placeholder="Cerca film..." id="search" v-model="searchText" />
+        <button @click="$emit('movieSearch', searchText)">Cerca</button>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'MyHeader',
+    data() {
+        return {
+            searchText: ''
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/MyMain.vue
@@ -0,0 +1,40 @@
+<template>
+    <main>
+        <div>
+
+        </div>
+    </main>
+</template>
+
+<script>
+
+    import axios from 'axios';
+
+export default {
+    name: 'MyMain',
+    props: {
+        searchedMovie: String
+    },
+    data() {
+        return {
+            movies: [],
+            endpoint: `https://api.themoviedb.org/3/search/movie?api_key=af6fbe18e93f8959cb08c13e609cab58&query=2001&language=it-IT`,
+        }
+    },
+    methods: {
+        getMovies() {
+            if (this.searchedMovie != '') {
+                axios.get(this.endpoint)
+                .then(response => {
+                    console.log(this.endpoint)
+                    this.movies.push(response.data.results);
+                })
+            }
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
